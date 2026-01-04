c3c1cce5741f6ca7ca7d1062c5809756ed744968
rushabh wagh <rushabhwagh125@gmail.com>
2026-01-04 11:26:27 +0530

    .

--- frontend/src/components/home/Navbar.jsx
+++ frontend/src/components/home/Navbar.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <div className='flex justify-between p-4 bg-white text-center align-middle'>
+      <img src="https://imgs.search.brave.com/F_cvQkPduvw28uV2_2cCbXAZDbgZyHEcVfeZSXuwhQM/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/cHJlbWl1bS12ZWN0/b3IvYWstc2lnbmF0/dXJlLWluaXRpYWwt/bG9nby10ZW1wbGF0/ZS12ZWN0b3Itc2ln/bmF0dXJlLWxvZ290/eXBlXzEwMDUzMjkt/MTcuanBnP3NlbXQ9/YWlzX2h5YnJpZCZ3/PTc0MCZxPTgw" alt="" />
       <h2 className='text-xl font-bold'>BookErase</h2>
       <div className='flex gap-15'>
         <Link to="#">Home</Link>
